7326bf976edbe6c57ca3e345cba1bfb81528c55b
SuyashGaneshKadam <9044suyash@gmail.com>
2024-01-19 15:46:53 +0530

    Nav Bar and Landing Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Common/Footer';
+import Header from './Components/Common/Header';
+import MainComponent from './Components/LandingPage/MainComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <MainComponent />
     </div>
   );
 }
---
+++ src/Components/LandingPage/MainComponent/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./styles.css";
+import Button from "../../Common/Button";
+import iphone from "../../../Assets/iphone.png";
+import gradient from "../../../Assets/gradient.png";
+import { motion } from "framer-motion";
+
+function MainComponent() {
+  return (
+    <div className="flex-info">
+      <div className="left-component">
+        <motion.h1
+          className="track-crypto-heading"
+          initial={{ opacity: 0, x: -500 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+        >
+          Track Crypto
+        </motion.h1>
+        <motion.h1
+          className="real-time-heading"
+          initial={{ opacity: 0, x: -500 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          Real Time.
+        </motion.h1>
+        <motion.p
+          className="info-text"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          Track crypto through a public api in real time. Visit the dashboard to
+          do so!
+        </motion.p>
+        <motion.div
+          className="btn-flex"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          <Button text={"Dashboard"} />
+          <Button text={"Share"} outlined={true} />
+        </motion.div>
+      </div>
+      <div className="phone-container">
+        <motion.img
+          src={iphone}
+          alt="Phone"
+          className="iphone"
+          initial={{ y: -20 }}
+          animate={{ y: 20 }}
+          transition={{
+            type: "smooth",
+            repeatType: "mirror",
+            duration: 1.5,
+            repeat: Infinity,
+          }}
+        />
+        <img src={gradient} alt="Gradient" className="gradient" />
+      </div>
+    </div>
+  );
+}
+
+export default MainComponent;
