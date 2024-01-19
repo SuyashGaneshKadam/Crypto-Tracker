2b51c2ab7dce71b286adc814e7f6d5e4be932b2c
SuyashGaneshKadam <9044suyash@gmail.com>
2024-01-20 04:45:41 +0530

    Completed Dashboard Page

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -2,24 +2,70 @@ import React, { useEffect, useState } from "react";
 import Header from "../Components/Common/Header";
 import TabsComponent from "../Components/Dashboard/Tabs";
 import axios from "axios";
+import Search from "../Components/Dashboard/Search";
+import PaginationComponent from "../Components/Dashboard/Pagination";
+import coins from "../Data/CoinsData";
+import Loader from "../Components/Common/Loader";
+import BackToTop from "../Components/Common/BackToTop";
 
 function DashboardPage() {
-  const [coins, setCoins] = useState([]);
-  
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
-      )
-      .then((res) => setCoins(res.data))
-      .catch((err) => console.log(err));
-  }, []);
+  // const [coins, setCoins] = useState([]);
+  const [paginatedCoins, setPaginatedCoins] = useState(coins.slice(0, 10));
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false); //Change to true when using API call
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+    var previousIndex = (value - 1) * 10;
+    setPaginatedCoins(coins.slice(previousIndex, previousIndex + 10));
+  };
+
+  const onSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  var filteredCoins = coins.filter(
+    (item) =>
+      item.name.toLowerCase().includes(search.toLowerCase()) ||
+      item.symbol.toLowerCase().includes(search.toLowerCase())
+  );
+
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
+  //     )
+  //     .then((res) => {
+  //       setCoins(res.data);
+  //       setPaginatedCoins(res.data.slice(0, 10));
+  //       setIsLoading(false);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
   return (
-    <div>
+    <>
       <Header />
-      <TabsComponent coins={coins }/>
-    </div>
+      <BackToTop/>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Search search={search} onSearchChange={onSearchChange} />
+          <TabsComponent coins={search ? filteredCoins : paginatedCoins} />
+          {!search && (
+            <PaginationComponent
+              page={page}
+              handlePageChange={handlePageChange}
+            />
+          )}
+        </div>
+      )}
+    </>
   );
 }
 
